a4ec97cdc9da23b990bec5251d8ff901a4b4a8c6
Regulus55 <155502886+Regulus55@users.noreply.github.com>
2024-11-16 15:31:07 +0900

    page folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Home from "./Home";
 import NotFound from "./NotFound";
-import Page1 from "./Page1";
-import Page2 from "./Page2";
-import Page3 from "./Page3";
-import Page4 from "./Page4";
-import Page5 from "./Page5";
+import Page1 from "./page/Page1";
+import Page2 from "./page/Page2";
+import Page3 from "./page/Page3";
+import Page4 from "./page/Page4";
+import Page5 from "./page/Page5";
 
 export { Home, NotFound, Page1, Page2, Page3, Page4, Page5 };
